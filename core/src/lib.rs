fe5f26a8e724e4049a3fc90c219d554080d33794
Alberto Centelles <centelles.alberto@gmail.com>
2024-09-12 15:15:55 +0200

    Add compliance circuit

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,23 +1,29 @@
 #![no_std]
 use k256::{
     elliptic_curve::{
-        group::ff::PrimeField,
+        group::{ff::PrimeField, GroupEncoding},
         hash2curve::{ExpandMsgXmd, GroupDigest},
     },
-    ProjectivePoint, Scalar, Secp256k1,
+    AffinePoint, ProjectivePoint, Scalar, Secp256k1,
 };
-use risc0_zkvm::sha::Digest;
-use risc0_zkvm::sha::DIGEST_BYTES;
-use risc0_zkvm::sha::{rust_crypto::Sha256 as Sha256Type, Impl, Sha256};
+use risc0_zkvm::sha::{
+    rust_crypto::Sha256 as Sha256Type, Impl, Sha256, DIGEST_BYTES, Digest};
+use risc0_zkvm::guest::env;
 use serde_big_array::BigArray;
+use serde::{Deserialize, Serialize};
+
 
 const DST: &[u8] = b"QUUX-V01-CS02-with-secp256k1_XMD:SHA-256_SSWU_RO_";
 
 /// Nullifier secret key
-#[derive(Clone, Debug, Default, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Copy, Debug, Default, Serialize, Deserialize)]
 pub struct Nsk(Digest);
 
 impl Nsk {
+
+    pub fn new(&self, nsk: Digest) -> Nsk {
+        Nsk(nsk)
+    }
     /// Compute the corresponding nullifier public key
     pub fn public_key(&self) -> Npk {
         let bytes: [u8; DIGEST_BYTES] = *self.0.as_ref();
@@ -26,7 +32,7 @@ impl Nsk {
 }
 
 /// Nullifier public key
-#[derive(Clone, Eq, PartialEq, Default, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Eq, PartialEq, Default, Serialize, Deserialize)]
 pub struct Npk(Digest);
 
 const LABEL_BYTES: usize = 32;
@@ -36,6 +42,8 @@ const FUNGIBLE_BYTES: usize = 32;
 const RSEED_BYTES: usize = 32;
 
 const FELT_BYTES: usize = 32;
+
+const DATA_BYTES: usize = 32;
 
 const RESOURCE_BYTES: usize = DIGEST_BYTES
     + LABEL_BYTES
@@ -65,7 +73,7 @@ pub struct Resource {
     pub nonce: Digest,
     // nullifier public key
     pub npk: Npk,
-    // randomness seed used to derive whatever randomness neede
+    // randomness seed used to derive whatever randomness needed
     pub rseed: [u8; RSEED_BYTES],
 }
 
@@ -84,6 +92,20 @@ impl Resource {
         bytes[DIGEST_BYTES..DIGEST_BYTES + LABEL_BYTES].clone_from_slice(&self.label);
         // Hash to a curve point
         Secp256k1::hash_from_bytes::<ExpandMsgXmd<Sha256Type>>(&[&bytes], &[DST]).unwrap()
+    }
+
+    pub fn psi(&self) -> Digest {
+        let mut bytes = [0u8; 2 * DIGEST_BYTES];
+        let mut offset: usize = 0;
+        // Write the random seed
+        bytes[offset..offset + DIGEST_BYTES].clone_from_slice(&self.rseed.as_ref());
+        offset += DIGEST_BYTES;
+        // Write the nonce
+        bytes[offset..offset + DIGEST_BYTES].clone_from_slice(&self.nonce.as_ref());
+        offset += DIGEST_BYTES;
+        assert_eq!(offset, 2 * DIGEST_BYTES);
+        // Now produce the hash
+        *Impl::hash_bytes(&bytes)
     }
 
     // // Resource deltas are used to reason about total quantities of different kinds of resources in transactions.
@@ -129,8 +151,24 @@ impl Resource {
     pub fn nullifier(&self, nsk: Nsk) -> Option<Digest> {
         // Make sure that the nullifier public key corresponds to the secret key
         if self.npk == nsk.public_key() {
-            // Derive the nullifier
-            Some(*Impl::hash_pair(&self.commitment(), &nsk.0))
+            let mut bytes = [0u8; 4 * DIGEST_BYTES];
+            let mut offset: usize = 0;
+            // Write the nullifier secret key
+            bytes[offset..offset + DIGEST_BYTES].clone_from_slice(&nsk.0.as_ref());
+            offset += DIGEST_BYTES;
+            // Write the nonce
+            bytes[offset..offset + DIGEST_BYTES].clone_from_slice(&self.nonce.as_ref());
+            offset += DIGEST_BYTES;
+            // Write psi
+            bytes[offset..offset + DIGEST_BYTES].clone_from_slice(&self.psi().as_ref());
+            offset += DIGEST_BYTES;
+            // Write the resource commitment
+            bytes[offset..offset + DIGEST_BYTES].clone_from_slice(&self.commitment().as_ref());
+            offset += DIGEST_BYTES;
+
+            assert_eq!(offset, 4 * DIGEST_BYTES);
+
+            Some(*Impl::hash_bytes(&bytes))
         } else {
             None
         }
@@ -177,7 +215,6 @@ where
 {
     #[serde(with = "BigArray")]
     pub auth_path: [(Node, bool); COMMITMENT_TREE_DEPTH],
-    pub position: u64,
 }
 
 impl<const COMMITMENT_TREE_DEPTH: usize, Node> MerklePath<COMMITMENT_TREE_DEPTH, Node>
@@ -185,10 +222,9 @@ where
     Node: Hashable + serde::Serialize + for<'de2> serde::Deserialize<'de2>,
 {
     /// Constructs a Merkle path directly from a path and position.
-    pub fn from_path(auth_path: [(Node, bool); COMMITMENT_TREE_DEPTH], position: u64) -> Self {
+    pub fn from_path(auth_path: [(Node, bool); COMMITMENT_TREE_DEPTH]) -> Self {
         MerklePath {
             auth_path,
-            position,
         }
     }
 
@@ -202,3 +238,53 @@ where
             })
     }
 }
+
+
+#[derive(Clone, serde::Serialize, serde::Deserialize)]
+pub struct Compliance<const COMMITMENT_TREE_DEPTH: usize> 
+{
+    pub input_resource: Resource,
+    #[serde(with = "BigArray")]
+    pub merkle_path: [(Digest, bool); COMMITMENT_TREE_DEPTH],
+    pub output_resource: Resource,
+    pub rcv: [u8; DATA_BYTES],
+    pub input_resource_logic_cm_r: [u8; DATA_BYTES],
+    pub output_resource_logic_cm_r: [u8; DATA_BYTES],
+    pub nsk: Nsk
+}
+
+impl<const COMMITMENT_TREE_DEPTH: usize> Compliance<COMMITMENT_TREE_DEPTH>
+{
+    pub fn check(&self) {
+        let nf = self.check_input_resource();
+        let cm = self.check_output_resource();
+        let merkle_root = self.check_merkle_tree_path(cm);
+        let delta = self.compute_delta_commitment();
+        env::commit(&(nf, cm, merkle_root, delta));
+    }    
+
+    fn check_input_resource(&self) -> Digest {
+        let nf = self.input_resource.nullifier(self.nsk).unwrap(); // Q: Do we want better error handling?
+        nf
+    }
+
+    fn check_output_resource(&self) -> Digest {
+        let cm = self.output_resource.commitment();
+        cm
+    }
+
+    fn check_merkle_tree_path(&self, cm: Digest) -> Digest {
+        // Check the input resource is along the merkle path and it can generate the root and publicise the root
+        let merkle_root = MerklePath::from_path(self.merkle_path).root(cm);
+        merkle_root
+    }
+
+    fn compute_delta_commitment(&self) -> [u8; DATA_BYTES] {
+        // Compute delta and make delta commitment public
+        // Comm(input_value - output_value)
+        let delta = self.input_resource.kind() * self.input_resource.quantity() - self.output_resource.kind() * self.output_resource.quantity();
+
+        delta.to_affine().to_bytes()[..].try_into().unwrap()
+    }
+}
+
