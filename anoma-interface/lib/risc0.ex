31108c4c6eac5646188becc6d10111c4455a6672
acentelles <centelles.alberto@gmail.com>
2024-10-23 17:44:35 +0100

    Cleanup

--- anoma-interface/lib/risc0.ex
+++ anoma-interface/lib/risc0.ex
@@ -1,10 +1,4 @@
 defmodule Risc0 do
-  # @spec risc0_vm_runner(binary(), binary()) ::
-  #         {binary(), [byte()], [byte()], [byte()]} | {:error, term()}
-  # defdelegate risc0_vm_runner(program_content, program_input),
-  #   to: Risc0.Risc0VM,
-  #   as: :risc0_vm_runner
-
   @spec prove([byte()], [byte()]) ::
           [byte()] | {:error, term()}
   defdelegate prove(env_bytes, elf),
@@ -64,50 +58,4 @@ defmodule Risc0 do
     @spec generate_nsk() :: list(byte()) | {:error, term()}
     defdelegate generate_nsk(), to: Risc0.Risc0Prover, as: :generate_nsk
 
-  # @spec get_output(list(byte())) ::
-  #         any() | {:error, term()}
-  # defdelegate get_output(env_bytes),
-  #   to: Risc0.Risc0Prover,
-  #   as: :get_output
-
-  # @spec sign(list(byte()), list(list(byte()))) ::
-  #         list(byte()) | {:error, term()}
-  # defdelegate sign(private_key_segments, messages),
-  #   to: Risc0.Risc0Prover,
-  #   as: :risc0_binding_sig_sign
-
-  # @spec sig_verify(list(list(byte())), list(list(byte())), list(byte())) ::
-  #         boolean() | {:error, term()}
-  # defdelegate sig_verify(pub_key_segments, messages, signature),
-  #   to: Risc0.Risc0Prover,
-  #   as: :risc0_binding_sig_verify
-
-  # @spec random_felt() ::
-  #         list(byte()) | {:error, term()}
-  # defdelegate random_felt(),
-  #   to: Risc0.Risc0Prover,
-  #   as: :risc0_random_felt
-
-  # @spec get_public_key(list(byte())) ::
-  #         list(byte()) | {:error, term()}
-  # defdelegate get_public_key(priv_key),
-  #   to: Risc0.Risc0Prover,
-  #   as: :risc0_get_binding_sig_public_key
-
-  # @spec sha256(list(byte()), list(byte())) ::
-  #         list(byte()) | {:error, term()}
-  # defdelegate sha256(x, y),
-  #   to: Risc0.Risc0Prover,
-  #   as: :sha256
-
-  # @spec get_program_hash(list(byte())) ::
-  #         list(byte()) | {:error, term()}
-  # defdelegate get_program_hash(pub_input),
-  #   to: Risc0.Risc0Prover,
-  #   as: :program_hash
-
-  # @spec felt_to_string(list(byte())) :: binary()
-  # defdelegate felt_to_string(felt),
-  #   to: Risc0.Risc0Prover,
-  #   as: :risc0_felt_to_string
 end
