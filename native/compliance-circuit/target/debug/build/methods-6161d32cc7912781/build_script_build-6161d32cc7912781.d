50d742bf49ff73045aaf83b2b64f4a4c1b086d92
acentelles <centelles.alberto@gmail.com>
2024-11-08 16:28:16 +0000

    Add only necessary targets

---
+++ native/compliance-circuit/target/debug/build/methods-6161d32cc7912781/build_script_build-6161d32cc7912781.d
@@ -0,0 +1,5 @@
+/Users/albertocentelles/Dropbox/heliax/aarm-risc0/native/compliance-circuit/target/debug/build/methods-6161d32cc7912781/build_script_build-6161d32cc7912781: methods/build.rs
+
+/Users/albertocentelles/Dropbox/heliax/aarm-risc0/native/compliance-circuit/target/debug/build/methods-6161d32cc7912781/build_script_build-6161d32cc7912781.d: methods/build.rs
+
+methods/build.rs:
