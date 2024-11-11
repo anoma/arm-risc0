ed3cff542dee61707b3156ddbf39808ac107b4bb
acentelles <centelles.alberto@gmail.com>
2024-11-11 11:35:19 +0000

    Add examples

--- native/compliance-circuit/target/debug/build/methods-2dacd0d24f782198/out/methods.rs
+++
@@ -1,4 +0,0 @@
-
-pub const COMPLIANCE_GUEST_ELF: &[u8] = include_bytes!("/Users/albertocentelles/Dropbox/heliax/aarm-risc0/native/compliance-circuit/target/riscv-guest/riscv32im-risc0-zkvm-elf/release/compliance_guest");
-pub const COMPLIANCE_GUEST_ID: [u32; 8] = [1418531829, 3784942378, 2702306531, 2314013923, 2826524244, 3896441791, 1493691200, 2301010845];
-pub const COMPLIANCE_GUEST_PATH: &str = "/Users/albertocentelles/Dropbox/heliax/aarm-risc0/native/compliance-circuit/target/riscv-guest/riscv32im-risc0-zkvm-elf/release/compliance_guest";
--- native/examples/compliance-circuit/host/src/main.rs
+++ native/examples/compliance-circuit/host/src/main.rs
@@ -1,4 +1,3 @@
-#![no_std]
 use aarm_core::{
     compliance::Compliance,
     constants::TREE_DEPTH,
@@ -6,12 +5,12 @@ use aarm_core::{
 };
 use methods::{COMPLIANCE_GUEST_ELF, COMPLIANCE_GUEST_ID};
 use risc0_zkvm::{default_prover, sha::Digest, ExecutorEnv};
-// use std::time::Instant;
+use std::time::Instant;
 use serde_bytes::ByteBuf;
 use bincode;
 const DATA_BYTES: usize = 32;
 pub fn main() {
-    // let prove_start_timer = Instant::now();
+    let prove_start_timer = Instant::now();
 
     let compliance: Compliance<32> = Compliance::<TREE_DEPTH>::default();
     let generic_env = GenericEnv {
@@ -29,10 +28,10 @@ pub fn main() {
     // Produce a receipt by proving the specified ELF binary.
     let receipt = prover.prove(env, COMPLIANCE_GUEST_ELF).unwrap().receipt;
 
-    // let prove_duration = prove_start_timer.elapsed();
-    // println!("Prove duration time: {:?}", prove_duration);
+    let prove_duration = prove_start_timer.elapsed();
+    println!("Prove duration time: {:?}", prove_duration);
 
-    // let extract_journal_start_timer = Instant::now();
+    let extract_journal_start_timer = Instant::now();
     // Extract journal of receipt
     let (_input_rl, _nf, _output_rl, _cm, _merkle_root, _delta): (
         Digest,
@@ -43,15 +42,15 @@ pub fn main() {
         [u8; DATA_BYTES],
     ) = receipt.journal.decode().unwrap();
 
-    // let extract_journal_duration = extract_journal_start_timer.elapsed();
-    // println!(
-    //     "Extract Journal duration time: {:?}",
-    //     extract_journal_duration
-    // );
+    let extract_journal_duration = extract_journal_start_timer.elapsed();
+    println!(
+        "Extract Journal duration time: {:?}",
+        extract_journal_duration
+    );
 
-    // let verify_start_timer = Instant::now();
+    let verify_start_timer = Instant::now();
 
     receipt.verify(COMPLIANCE_GUEST_ID).unwrap();
-    // let verify_duration = verify_start_timer.elapsed();
-    // println!("Verify duration time: {:?}", verify_duration);
+    let verify_duration = verify_start_timer.elapsed();
+    println!("Verify duration time: {:?}", verify_duration);
 }
