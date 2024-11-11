cb284a05eebe542a2e5e6511c4053cf750eb474d
acentelles <centelles.alberto@gmail.com>
2024-11-11 13:32:23 +0000

    Add compliance circuit binaries

---
+++ native/examples/compliance-circuit/target/debug/build/methods-2dacd0d24f782198/out/methods.rs
@@ -0,0 +1,4 @@
+
+pub const COMPLIANCE_GUEST_ELF: &[u8] = include_bytes!("/Users/albertocentelles/Dropbox/heliax/aarm-risc0/native/examples/compliance-circuit/target/riscv-guest/riscv32im-risc0-zkvm-elf/release/compliance_guest");
+pub const COMPLIANCE_GUEST_ID: [u32; 8] = [2712213410, 2289638466, 1880470580, 1814499758, 1010248647, 377240384, 421482662, 1407330473];
+pub const COMPLIANCE_GUEST_PATH: &str = "/Users/albertocentelles/Dropbox/heliax/aarm-risc0/native/examples/compliance-circuit/target/riscv-guest/riscv32im-risc0-zkvm-elf/release/compliance_guest";
