55f6ee442fffb06b35008f672641bf5a2c00ad2c
acentelles <centelles.alberto@gmail.com>
2024-11-13 14:26:29 +0000

    Add logic instance struc

--- native/aarm_core/src/lib.rs
+++ native/aarm_core/src/lib.rs
@@ -5,11 +5,4 @@ pub mod merkle_path;
 pub mod nullifier;
 pub mod resource;
 pub mod utils;
-
-// pub use resource::Resource;
-// pub use compliance::Compliance;
-// pub use constants::{TREE_DEPTH, DEFAULT_BYTES, RESOURCE_BYTES};
-// pub use merkle_path::MerklePath;
-// pub use nullifier::{Npk, Nsk};
-// pub use utils::GenericEnv;
-// pub use encryption::{Ciphertext, projective_point_to_bytes, bytes_to_projective_point, SecretKey, generate_public_key, random_keypair};
+pub mod logic_instance;
