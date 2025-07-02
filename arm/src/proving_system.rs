4afe5031bf807da63e8666e8ace19621477f395c
Xuyang Song <xuyangsong@hotmail.com>
2025-07-02 22:51:05 +0800

    support more proof types

--- arm/src/proving_system.rs
+++ arm/src/proving_system.rs
@@ -7,8 +7,7 @@ use serde::{de::DeserializeOwned, Serialize};
 
 // It takes a proving key and a witness, and returns the proof and the instance
 pub fn prove<T: Serialize>(proving_key: &[u8], witness: &T) -> (Vec<u8>, Vec<u8>) {
-    // TODO: add other proving options(STARK, COMPOSITION)
-    let receipt = groth16_prove(witness, proving_key);
+    let receipt = prove_inner(witness, proving_key);
 
     let proof = bincode::serialize(&receipt.inner).unwrap();
     let instance = receipt.journal.bytes;
@@ -44,21 +43,36 @@ pub fn convert_image_id_to_bytes(id: &[u32]) -> Vec<u8> {
     bytes
 }
 
-// TODO: handle errors properly
-fn groth16_prove<T: Serialize>(witness: &T, proving_key: &[u8]) -> Receipt {
+fn prove_inner<T: Serialize>(witness: &T, proving_key: &[u8]) -> Receipt {
     let env = ExecutorEnv::builder()
         .write(witness)
         .unwrap()
         .build()
         .unwrap();
 
+    // Defualt prover: it generates succinct receipts, which are constant size in the length of
+    // execution.
+    #[cfg(feature = "succinct_prover")]
+    let prover_opts = ProverOpts::succinct();
+
+    // The fastest prover options.  Receipt will be linear in length of the
+    // execution, and does not support compression via recursion.
+    #[cfg(feature = "fast_prover")]
+    let prover_opts = ProverOpts::fast();
+
+    // It generates composite receipts, linear in the length of the execution,
+    // and supports compression via recursion.
+    #[cfg(feature = "composite_prover")]
+    let prover_opts = ProverOpts::composite();
+
+    // generates Groth16 receipts which are constant size in the length of the
+    // execution and small enough to verify on blockchains, like Ethereum. Only
+    // supported for x86_64 Linux with Docker installed.
+    #[cfg(feature = "groth16_prover")]
+    let prover_opts = ProverOpts::groth16();
+
     default_prover()
-        .prove_with_ctx(
-            env,
-            &VerifierContext::default(),
-            proving_key,
-            &ProverOpts::groth16(),
-        )
+        .prove_with_ctx(env, &VerifierContext::default(), proving_key, &prover_opts)
         .unwrap()
         .receipt
 }
