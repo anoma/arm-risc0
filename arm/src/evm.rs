10a62b031ac6673349d7802b2f24fc5ec495129f
Xuyang Song <xuyangsong@hotmail.com>
2025-08-22 14:41:17 +0800

    add EvmResource

--- arm/src/evm.rs
+++ arm/src/evm.rs
@@ -1,6 +1,44 @@
-// use alloy_primitives::address;
+use crate::resource::Resource as ArmResource;
+use alloy_primitives::{B256, U256};
 use alloy_sol_types::sol;
 use alloy_sol_types::SolValue;
+sol! {
+    struct Resource {
+        bytes32 logicRef;
+        bytes32 labelRef;
+        bytes32 valueRef;
+        bytes32 nullifierKeyCommitment;
+        uint256 quantity;
+        uint256 nonce;
+        uint256 randSeed;
+        bool ephemeral;
+    }
+}
+
+impl Resource {
+    pub fn encode(&self) -> Vec<u8> {
+        self.abi_encode()
+    }
+
+    pub fn decode(encoded: &[u8]) -> Option<Self> {
+        Self::abi_decode(encoded).ok()
+    }
+}
+
+impl From<ArmResource> for Resource {
+    fn from(r: ArmResource) -> Self {
+        Self {
+            logicRef: B256::from_slice(&r.logic_ref),
+            labelRef: B256::from_slice(&r.label_ref),
+            quantity: U256::from(r.quantity),
+            valueRef: B256::from_slice(&r.value_ref),
+            ephemeral: r.is_ephemeral,
+            nonce: U256::from_le_slice(r.nonce.as_slice()),
+            nullifierKeyCommitment: B256::from_slice(r.nk_commitment.inner()),
+            randSeed: U256::from_le_slice(r.rand_seed.as_slice()),
+        }
+    }
+}
 
 sol! {
     struct ForwarderCalldata {
@@ -39,7 +77,7 @@ impl ForwarderCalldata {
 }
 
 #[test]
-fn forward_call_data() {
+fn forward_call_data_test() {
     // Example data
     let addr = "0x1111111111111111111111111111111111111111";
     let input = hex::decode("1122").unwrap();
@@ -58,4 +96,26 @@ fn forward_call_data() {
     assert_eq!(data.untrustedForwarder, decoded_data.untrustedForwarder);
     assert_eq!(data.input, decoded_data.input);
     assert_eq!(data.output, decoded_data.output);
+}
+
+#[test]
+fn evm_resource_test() {
+    let arm_resource = ArmResource::default();
+    let evm_resource: Resource = arm_resource.clone().into();
+    let encoded_resource = evm_resource.encode();
+    let decoded_resource = Resource::decode(&encoded_resource).unwrap();
+    assert_eq!(arm_resource.logic_ref, decoded_resource.logicRef.as_slice());
+    assert_eq!(arm_resource.label_ref, decoded_resource.labelRef.as_slice());
+    assert_eq!(arm_resource.value_ref, decoded_resource.valueRef.as_slice());
+    assert_eq!(arm_resource.nonce, decoded_resource.nonce.as_le_slice());
+    assert_eq!(
+        arm_resource.rand_seed,
+        decoded_resource.randSeed.as_le_slice()
+    );
+    assert_eq!(arm_resource.is_ephemeral, decoded_resource.ephemeral);
+    assert_eq!(U256::from(arm_resource.quantity), decoded_resource.quantity);
+    assert_eq!(
+        arm_resource.nk_commitment.inner(),
+        decoded_resource.nullifierKeyCommitment.as_slice()
+    );
 }
