af43e5d659d3b5e0651c481b4aad137e8451ec44
Justin R. Evans <jurevans@gmail.com>
2025-09-15 04:02:19 -0400

    updating prove feature gates to build to wasm32

--- arm/src/action.rs
+++ arm/src/action.rs
@@ -1,14 +1,17 @@
 use crate::{
     action_tree::MerkleTree,
-    compliance::{ComplianceInstance, ComplianceWitness},
+    compliance::ComplianceInstance,
     compliance_unit::ComplianceUnit,
-    delta_proof::DeltaWitness,
-    logic_proof::{LogicProver, LogicVerifier, LogicVerifierInputs},
-    nullifier_key::NullifierKey,
-    resource::Resource,
+    logic_proof::{LogicVerifier, LogicVerifierInputs},
 };
 use k256::ProjectivePoint;
 use serde::{Deserialize, Serialize};
+
+#[cfg(feature = "prove")]
+use crate::{
+    compliance::ComplianceWitness, delta_proof::DeltaWitness, logic_proof::LogicProver,
+    nullifier_key::NullifierKey, resource::Resource,
+};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Action {
@@ -106,6 +109,7 @@ impl Action {
     }
 }
 
+#[cfg(feature = "prove")]
 pub fn create_an_action(nonce: u8) -> (Action, DeltaWitness) {
     use crate::logic_proof::TestLogic;
 
@@ -156,6 +160,7 @@ pub fn create_an_action(nonce: u8) -> (Action, DeltaWitness) {
     (action, delta_witness)
 }
 
+#[cfg(feature = "prove")]
 pub fn create_multiple_actions(n: usize) -> (Vec<Action>, DeltaWitness) {
     let mut actions = Vec::new();
     let mut delta_witnesses = Vec::new();
