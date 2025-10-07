d49e9d559c7a7baa550a8d434516873cf972ad2c
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-10-07 21:36:18 +0200

    chore: manually update of SIMPLE_TRANSFER_ID

--- examples/simple_transfer_application/app/src/lib.rs
+++ examples/simple_transfer_application/app/src/lib.rs
@@ -24,7 +24,7 @@ use simple_transfer_witness::{
 pub const SIMPLE_TRANSFER_ELF: &[u8] = include_bytes!("../elf/simple-transfer-guest.bin");
 lazy_static! {
     pub static ref SIMPLE_TRANSFER_ID: Digest =
-        Digest::from_hex("81f8104fe367f5018a4bb0b259531be9ab35d3f1d51dea46c204bee154d5ee9e")
+        Digest::from_hex("5fd6b96958d92fcef90a8284480b1d297868f52c62ecd5167e0806760362ccc5")
             .unwrap();
 }
 
