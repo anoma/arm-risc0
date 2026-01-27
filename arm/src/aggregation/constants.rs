a01475c2a329d9d0da9eea14c5d67549e0e72b5e
Xuyang Song <xuyangsong@hotmail.com>
2026-01-27 15:53:39 +0800

    rename batch_aggregation

--- arm/src/aggregation/constants.rs
+++ arm/src/aggregation/constants.rs
@@ -5,9 +5,9 @@ use lazy_static::lazy_static;
 use risc0_zkvm::Digest;
 
 /// Batch aggregation proving key / batch aggregation guest ELF binary
-pub const BATCH_AGGREGATION_PK: &[u8] = include_bytes!("../../elfs/batch_aggregation.bin");
+pub const BATCH_AGGREGATION_PK: &[u8] = include_bytes!("../../elfs/batch-aggregation-guest.bin");
 
 lazy_static! {
     /// Batch aggregation verification key / Batch aggregation image id.
-    pub static ref BATCH_AGGREGATION_VK: Digest = Digest::from_hex("213b3f40d7c113c1a012072fcd791fa44bf5166a2300121630bd3228e2b00827").unwrap();
+    pub static ref BATCH_AGGREGATION_VK: Digest = Digest::from_hex("5ca0cbd4d5c267f42e0883b1ae7a28689d792230d9c4c61ca4f5df56aaf5fede").unwrap();
 }
--- arm_circuits/batch_aggregation/src/main.rs
+++ arm_circuits/batch_aggregation/src/main.rs
@@ -6,18 +6,18 @@ pub fn main() {
 // using the locally compiled circuit.
 #[test]
 fn print_aggregation_elf_id() {
-    use batch_aggregation_methods::{BATCH_AGGREGATION_ELF, BATCH_AGGREGATION_ID};
+    use batch_aggregation_methods::{BATCH_AGGREGATION_GUEST_ELF, BATCH_AGGREGATION_GUEST_ID};
     // Write the elf binary to a file
     std::fs::write(
-        "../../arm/elfs/batch_aggregation.bin",
-        BATCH_AGGREGATION_ELF,
+        "../../arm/elfs/batch-aggregation-guest.bin",
+        BATCH_AGGREGATION_GUEST_ELF,
     )
     .expect("Failed to write batch aggregation ELF binary");
 
     // Print the ID
     use risc0_zkvm::sha::Digest;
     println!(
-        "BATCH_AGGREGATION_ID: {:?}",
-        Digest::from(BATCH_AGGREGATION_ID)
+        "BATCH_AGGREGATION_GUEST_ID: {:?}",
+        Digest::from(BATCH_AGGREGATION_GUEST_ID)
     );
 }
