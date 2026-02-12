e2ab5680c76d39bd89d1f6073b173629d255f9e4
AHartNtkn <soobtoob@gmail.com>
2026-02-12 03:03:00 -0500

    Make risc0-zkvm, k256, lazy_static optional; add standalone Digest
    - risc0-zkvm, k256, lazy_static become optional dependencies
    - New `zkvm` and `k256` feature flags control their inclusion
    - Standalone Digest type (wire-compatible with risc0) for non-zkvm builds
    - Feature-conditional DeltaProof/DeltaWitness via delta_types module
    - Feature-gate all modules in lib.rs appropriately

--- arm/src/lib.rs
+++ arm/src/lib.rs
@@ -2,29 +2,41 @@
 
 #![deny(missing_docs)]
 
+#[cfg(not(feature = "zkvm"))]
+mod digest;
+#[cfg(not(feature = "zkvm"))]
+pub use digest::{Digest, DIGEST_BYTES, DIGEST_WORDS};
+
+#[cfg(feature = "zkvm")]
+pub use risc0_zkvm::sha::{DIGEST_BYTES, DIGEST_WORDS};
+#[cfg(feature = "zkvm")]
+pub use risc0_zkvm::Digest;
+
 #[cfg(feature = "transaction")]
 pub mod action;
+#[cfg(feature = "zkvm")]
 pub mod action_tree;
 #[cfg(any(feature = "compliance_circuit", feature = "aggregation_circuit"))]
 pub mod compliance;
 #[cfg(feature = "transaction")]
 pub mod compliance_unit;
-#[cfg(feature = "transaction")]
 pub mod constants;
-#[cfg(feature = "transaction")]
+#[cfg(all(feature = "transaction", feature = "k256"))]
 pub mod delta_proof;
+#[cfg(feature = "transaction")]
+pub mod delta_types;
 pub mod error;
 pub mod logic_instance;
-#[cfg(feature = "transaction")]
+#[cfg(all(feature = "transaction", feature = "zkvm", feature = "k256"))]
 pub mod logic_proof;
 pub mod merkle_path;
 pub mod nullifier_key;
-#[cfg(feature = "transaction")]
+#[cfg(all(feature = "transaction", feature = "zkvm"))]
 pub mod proving_system;
+#[cfg(all(feature = "zkvm", feature = "k256"))]
 pub mod resource;
+#[cfg(all(feature = "zkvm", feature = "k256"))]
 pub mod resource_logic;
 #[cfg(feature = "transaction")]
 pub mod transaction;
 pub mod utils;
-
-pub use risc0_zkvm::Digest;
